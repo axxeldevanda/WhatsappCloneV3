3655894c3a21a5883e7004a364f927d707485716
Axxel <axxeldevanda@gmail.com>
2019-10-14 19:29:19 +0700

    asd

---
+++ app/src/main/java/com/oldlex/whatsappclone/FindFriendsActivity.java
@@ -0,0 +1,33 @@
+package com.oldlex.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    private Toolbar mToolBar;
+    private RecyclerView FindFriendsRecyclerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_friends);
+
+        FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
+
+        mToolBar = (Toolbar) findViewById(R.id.find_friens_toolbar);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Find Friends");
+
+
+
+    }
+}
